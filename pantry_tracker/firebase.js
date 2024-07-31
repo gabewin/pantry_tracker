fa47f2610843265e3bb6ae565a1fe911a3501c48
gabewin <109232844+gabewin@users.noreply.github.com>
2024-07-31 14:29:32 +0200

    Update firebase.js

--- pantry_tracker/firebase.js
+++ pantry_tracker/firebase.js
@@ -5,14 +5,6 @@ import {getFirestore} from 'firebase/firestore'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDmjSxMXzRgoTOpcWVkqNjyv-uQ_wW8U5k",
-  authDomain: "hspantry-a1fa6.firebaseapp.com",
-  projectId: "hspantry-a1fa6",
-  storageBucket: "hspantry-a1fa6.appspot.com",
-  messagingSenderId: "707901383961",
-  appId: "1:707901383961:web:223706e809297efec1d188"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
