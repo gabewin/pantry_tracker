1c12fd49acab95021d6c86c76653ba57f5e12c73
gabewin <109232844+gabewin@users.noreply.github.com>
2024-08-08 18:18:58 +0200

    Update firebase.js

--- pantry_tracker/firebase.js
+++ pantry_tracker/firebase.js
@@ -1,4 +1,4 @@
-T// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
